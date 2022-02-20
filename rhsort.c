55025106e02f4e2570cf8cf1a3776545a2ae3da5
Marshall Lochbaum <mwlochbaum@gmail.com>
2022-02-20 18:50:36 -0500

    Comments

--- rhsort.c
+++ rhsort.c
@@ -6,15 +6,20 @@ typedef size_t U;
 #define LIKELY(X) __builtin_expect(X,1)
 #define RARE(X) __builtin_expect(X,0)
 
+// Minimum size to steal from buffer
 const U BLOCK = 16;
 
+// Merge arrays of length l and n-l starting at a, using buffer aux.
 void merge(T *a, U l, U n, T *aux) {
+  // Easy cases when the merge can be avoided
+  // If the buffer helping at all, most merges go through these
   if (a[l-1] <= a[l]) return;
   if (a[n-1] < a[0] && l+l==n) {
     T *b = a+l;
     for (U i=0; i<l; i++) { T t=a[i]; a[i]=b[i]; b[i]=t; }
     return;
   }
+  // Ordinary merge code, not fast or anything
   memcpy(aux, a, l*sizeof(T));
   for (U ai=0, bi=l, i=0; i<bi; i++) {
     if (bi>=n || aux[ai]<=a[bi])
@@ -24,38 +29,55 @@ void merge(T *a, U l, U n, T *aux) {
   }
 }
 
+// The main attraction. Sort array of ints with length n.
 void rhsort32(void *array, U n) {
-  T *x = array, *xb=x;
+  T *x = array, *xb=x;  // Stolen blocks go to xb
+
+  // Find the range.
   T min=x[0], max=min;
   for (U i=1; i<n; i++) {
     T e=x[i]; if (e<min) min=e; if (e>max) max=e;
   }
-  T s = max+1;
-  U r = (U)(unsigned int)(max-min) + 1;
-  U sh = 0;
-  while (r>4*n) { sh++; r>>=1; }
+
+  // Planning for the buffer
+  T s = max+1;                          // Sentinel value
+  U r = (U)(unsigned int)(max-min) + 1; // Size of range
+  U sh = 0;                             // Contract to fit range
+  while (r>4*n) { sh++; r>>=1; }        // Shrink to stay at O(n) memory
+  // Goes down to BLOCK once we know we have to merge
   U threshold = 2*BLOCK;
-  U sz = r + threshold;
+  U sz = r + threshold;                 // Buffer size
+
+  // Allocate buffer, and fill with sentinels
   T *aux = malloc((sz>n?sz:n)*sizeof(T)); // >=n for merges later
   for (U i=0; i<sz; i++) aux[i] = s;
+
+  // Main loop: insert array entries into buffer
   #define POS(E) ((U)((E)-min) >> sh)
   for (U i=0; i<n; i++) {
-    T e = x[i];
-    U j = POS(e), j0=j, f=j;
-    T h = aux[f];
-    if (LIKELY(h==s)) { aux[f]=e; continue; }
+    T e = x[i];               // Entry to be inserted
+    U j = POS(e);             // Target position
+    T h = aux[j];             // What's there?
+    // Common case is that it's empty (marked with sentinel s)
+    if (LIKELY(h==s)) { aux[j]=e; continue; }
+
     // Collision: find size of chain and position in it
+    U j0=j, f=j;
     do {
-      j += e>=h;
-      h = aux[++f];
-    } while (RARE(h!=s));
+      j += e>=h;          // If we have to move past that entry
+      h = aux[++f];       // Next one
+    } while (RARE(h!=s)); // Until the end of the chain
+    // We'll insert at j. Move the values after that one forward.
     for (U t=f; t>j; t--) aux[t] = aux[t-1];
     aux[j]=e;
-    f += 1;
+    f += 1;  // To account for just-inserted e
+
     // Bad collision: send chain back to x
     if (RARE(f-j0 >= threshold)) {
       threshold = BLOCK;
-      while (j0 && aux[j0-1]!=s) j0--; // Ensure stability
+      // Find the beginning of the chain (required for stability)
+      while (j0 && aux[j0-1]!=s) j0--;
+      // Move as many blocks from it as possible
       T *hj = aux+j0, *hf = aux+f;
       while (hj <= hf-BLOCK) {
         for (U i=0; i<BLOCK; i++) { xb[i]=hj[i]; hj[i]=s; }
@@ -72,10 +94,14 @@ void rhsort32(void *array, U n) {
     }
   }
   #undef POS
+
+  // Move all values from the buffer back to the array
+  // The if (...) xt++ part should be done branchlessly
   while (aux[--sz] == s); sz++;
-  T *xt=xb;
-  for (U i=0; i<sz; i++) if ((*xt=aux[i])!=s) xt++;
-  U l = xb-x;
+  T *xt=xb; for (U i=0; i<sz; i++) if ((*xt=aux[i])!=s) xt++;
+
+  // Merge stolen blocks back in if necessary
+  U l = xb-x;  // Size of those blocks
   if (l) {
     // Sort x[0..l]
     for (U w=BLOCK; w<l; w*=2)
@@ -84,5 +110,5 @@ void rhsort32(void *array, U n) {
     // And merge with the rest of x
     merge(x, l, n, aux);
   }
-  free(aux);
+  free(aux);  // All done!
 }
