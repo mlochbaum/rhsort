0df67d7a676d07f1eb9768d7a10bbe298913c55c
Marshall Lochbaum <mwlochbaum@gmail.com>
2023-02-04 11:34:38 -0500

    Update wolfsort benchmark testing setup (but not results yet)

--- wolfbench.sh
+++ wolfbench.sh
@@ -7,6 +7,7 @@ then
   git clone https://github.com/scandum/wolfsort.git
   cd wolfsort/src
   git apply ../../wolfbench.diff
+  ln -s ../../rhsort.c .
   wget https://raw.githubusercontent.com/orlp/pdqsort/b1ef26a55cdb60d236a5cb199c4234c704f46726/pdqsort.h
   wget https://raw.githubusercontent.com/skarupke/ska_sort/2c14d4bf7b667032ed28a481065f721385e33849/ska_sort.hpp
 else
